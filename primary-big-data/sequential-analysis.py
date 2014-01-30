9ba2119f782d9facf16aa1ee6f2ec7026a21c8e1
Suprita Shankar <supritashankar@ss.local>
2014-01-29 22:37:56 -0500

    woot! after a short break from Git

---
+++ primary-big-data/sequential-analysis.py
@@ -0,0 +1,13 @@
+ss:project1.2 supritashankar$ vim solution.py 
+
+#!/usr/bin/python
+import sys
+import re
+
+def main(argv):
+        pattern1 = re.compile(r'^en ')
+        pattern2 = re.compile(r'^en [a-z]')
+        pattern3 = re.compile(r'^en (Media|Special|Talk|User|User_talk|Project|Project_talk|File|File_talk|MediaWiki|MediaWiki_talk|Template|Template_talk|Help|Help_talk|Category|Category_talk|Portal|Wikipedia|Wikipedia_talk)')
+        pattern4 = re.compile(r'\.(ico|jpg|gif|png|JPG|PNG|txt) [0-9]')
+        pattern5 = re.compile(r'^en (Main_Page|Hypertext_Transfer_Protocol|404_error/|Favicon.ico|Search) [0-9]')
+        data = []
