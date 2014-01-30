492a7cf5441079ef249159c3a530522ae327378e
Suprita Shankar <suprita.shankar@gmail.com>
2014-01-29 22:42:33 -0500

    check if commits work well part-2

--- primary-big-data/sequential-analysis.py
+++ primary-big-data/sequential-analysis.py
@@ -10,4 +10,3 @@ def main(argv):
         pattern3 = re.compile(r'^en (Media|Special|Talk|User|User_talk|Project|Project_talk|File|File_talk|MediaWiki|MediaWiki_talk|Template|Template_talk|Help|Help_talk|Category|Category_talk|Portal|Wikipedia|Wikipedia_talk)')
         pattern4 = re.compile(r'\.(ico|jpg|gif|png|JPG|PNG|txt) [0-9]')
         pattern5 = re.compile(r'^en (Main_Page|Hypertext_Transfer_Protocol|404_error/|Favicon.ico|Search) [0-9]')
-        data = []
